ea2040e8d2d531db069e2fb3da0a24bba23cd93f
fernandomurcia1 <fermupa1982@gmail.com>
2019-10-23 11:49:17 -0500

    Modificacion
    Se cre la clase ClaseFinal se crean variables de tipo final lo que permite que el valor de las variables no se pueda modificar.

---
+++ src/palabrafinal/ClaseFinal.java
@@ -0,0 +1,21 @@
+package palabrafinal;
+
+/**
+ *
+ * @author fernando.murcia
+ */
+
+// Clase marcada como final
+public final class ClaseFinal {
+    
+    // Variables creadas como finales los valores de las variables no pueden ser modicados
+    
+    public static final int varPrimitivo = 10;
+    
+    public static final Persona persona = new Persona();
+    
+    public final void metodoFinal(){
+        
+    }
+    
+}
--- src/palabrafinal/Palabrafinal.java
+++ src/palabrafinal/Palabrafinal.java
@@ -7,7 +7,11 @@ package palabrafinal;
 public class Palabrafinal {
 
     public static void main(String[] args) {
+        System.out.println("Nombre Persona: " + ClaseFinal.persona.getNombre());
         
+        ClaseFinal.persona.setNombre("Luciana");
+        
+        System.out.println("Nombre persona: " + ClaseFinal.persona.getNombre());
     }
     
 }
