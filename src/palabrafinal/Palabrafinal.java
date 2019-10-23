d9e9b6325377c7f749c29b6f2b2f5661db376347
fernandomurcia1 <fermupa1982@gmail.com>
2019-10-23 10:36:23 -0500

    Initial commit

---
+++ src/palabrafinal/Palabrafinal.java
@@ -0,0 +1,13 @@
+package palabrafinal;
+
+/**
+ *
+ * @author fernando.murcia
+ */
+public class Palabrafinal {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/palabrafinal/Persona.java
@@ -0,0 +1,28 @@
+package palabrafinal;
+
+/**
+ *
+ * @author fernando.murcia
+ */
+public class Persona {
+    
+    // Se crea atributos de tipo privado y string y se genera los metos get and set
+    
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+    
+  
+    
+    
+    
+}
